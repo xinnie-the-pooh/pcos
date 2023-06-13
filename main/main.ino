3014e2a9f1735c27c6327c9414581d0e978e12d2
Winston Smith <zengfanhua@live.com>
2023-06-14 01:39:16 +0800

    Create main.ino

---
+++ main/main.ino
@@ -0,0 +1,11 @@
+#include <EEPROM.h>
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+EEPROM.write(1,5);
+EEPROM.write(2,9);
+}
