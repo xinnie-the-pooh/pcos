c94c3dd69504c57e539ae39aa6076767773114df
Winston Smith <zengfanhua@live.com>
2023-10-04 14:50:49 +0800

    修正

--- pcos/__vm/.pcos.vsarduino.h
+++ pcos/__vm/.pcos.vsarduino.h
@@ -5,17 +5,18 @@
 			all non-arduino files created by visual micro and all visual studio project or solution files can be freely deleted and are not required to compile a sketch (do not delete your own code!).
 			note: debugger breakpoints are stored in '.sln' or '.asln' files, knowledge of last uploaded breakpoints is stored in the upload.vmps.xml file. Both files are required to continue a previous debug session without needing to compile and upload again
 	
-	Hardware: ATmega2560 (Mega 2560) (Arduino Mega)                                                            (mega_atmega2560), Platform=avr, Package=arduino
+	Hardware: Arduino Uno                                                                                                             (uno), Platform=avr, Package=arduino
 */
 
-#define __AVR_ATmega2560__
+#define __AVR_ATmega328p__
+#define __AVR_ATmega328P__
 #define ARDUINO 108019
 #define ARDUINO_MAIN
 #define F_CPU 16000000L
 #define __AVR__
 #define F_CPU 16000000L
 #define ARDUINO 108019
-#define ARDUINO_AVR_MEGA2560
+#define ARDUINO_AVR_UNO
 #define ARDUINO_ARCH_AVR
 #include "pins_arduino.h" 
 #include "arduino.h"
--- pcos/pcos.ino
+++ pcos/pcos.ino
@@ -1,111 +1,68 @@
 #define btm 300  //串口倍率
-
-#ifdef __AVR_ATmega2560__  //引脚数量
+#if defined(__AVR_ATmega2560__)  //引脚数量
 #define dpinsnum 53
 #define apinsnum 15
-#define pwmse 1
-
 #endif
 
+#if  defined(__AVR_ATmega328P__)||defined(__AVR_ATmega168__) //
+#define dpinsnum 13
+#define apinsnum 7
+#endif
 #include <EEPROM.h>
 String Str;
 //byte mul_1 = 1;
 //byte mul_2 = 2;
 //byte at_flag = 0;
 int sped = 9600; //defalut
-byte count;
+/*byte count;*/
 //int config = SERIAL_8N1;
 byte ser0;//read
 byte ser1;
 byte ser2;
-byte newflag;
 byte acmode;
 /*int editmode;*/
-byte nm;//write
 void(* resetFunc) (void) = 0;
 void setup(void)
 {
-	if (ser1!=255&&ser2!=255)newflag=1;
 	acmode = 0;
 	//  editmode = 0;
 	//0-20保留给状态
 	ser0 = EEPROM.read(0);//
 	ser1 = EEPROM.read(1);
 	ser2 = EEPROM.read(2);
-	
 	if (ser0!=255) sped=btm *pow(2,ser0);//存储器非空装入波特率
 	Serial.begin(sped);
 	Serial.print("LOAD UART PROFILE DONE @"); Serial.println(sped);
-	
-	if(newflag==1){//读取机器码
+	if(ser1!=255&&ser2!=255==1){//读取机器码 newflag
 		ack();
 		Serial.println(" : AC MODE ON");
 		acmode = 1;
-		for (int p=0;p<=dpinsnum;p++)
-		{
-			pinMode(p,EEPROM.read(p+20));//引脚配置方法 从20-100
-		}
-		
+		for (byte p=0;p<=dpinsnum;p++)pinMode(p,EEPROM.read(p+20));//引脚配置方法 从20-100
 	}
-	
 	else Serial.println("AC MODE FAILED");
 	delay(1000);
-
 }
-
-// Add the main program code into the continuous loop() function
 void loop(void)
 {
 	nod(Str);
 	if (Str != "") {
-		Serial.print("INPUT->");
-		Serial.println(Str);
-		if (acmode == 0) {
-			at();
-			//      ac();
-			ch();
-			help();
-		}
-		//  else// if (acmode == 1)
-		//    {
-		ins();
-		//      help();
-		//    }
+		Serial.print("INPUT->"); Serial.println(Str);//回显
+		//未激活地址码可用命令:at模式:串口\地址\重启\展示\帮助,
+		if (acmode == 0)at();
+		if (acmode == 1)ins();
+		help;//全局放在外边
 	}
-	//herer= gose correct program
-	delay(100); // 延时 100 毫秒
+	/*	delay(100); // 延时 100 毫秒,这个时延在存在双状态周期时不需要*/
 }
 //有响应内部命令
 //show
-void pps(void){
+void shown(void){
 	if ((byte)Str[3] == 'S' && (byte)Str[4] == 'H' && (byte)Str[5] == 'O' && (byte)Str[6] == 'W') {
 		ack();
 		Serial.print(ser0);Serial.print(";"); Serial.print(ser1);Serial.print(";"); Serial.print(ser2);Serial.println(";");
 		/*	Serial.println("UART SPEED ; ADDR HIGH ; ADDR LOW");*/
-		
 	}
 }
-// 	void ex(void){
-//
-// 		if ((byte)Str[3] == 'E' && (byte)Str[4] == 'X' && (byte)Str[5] == 'I' && (byte)Str[6] == 'T') {
-// 			ask(0);
-// 			//        if (editmode==0){
-// 			acmode = 0;
-// 			Serial.println("AC MODE OFF");
-// 			//        }
-// 			//        if (editmode==1){
-// 			//        editmode = 0;
-// 			//        Serial.println(" EDIT MODE QUIT");
-// 			//      }
-// 			//  if ((byte)Str[3] == 'E' && (byte)Str[4] == 'D' && (byte)Str[5] == 'I' && (byte)Str[6] == 'T') {
-// 			//        ask(0);
-// 			//        Serial.println(" EDITING GOES");
-// 			//       editmode=1;
-// 			//
-// 			//      }
-// 		}
-// 	}
-// }
 //rst
 void rst(void) {
 	if ((byte)Str[3] == 'R' && (byte)Str[4] == 'S' && (byte)Str[5] == 'T') {
@@ -114,22 +71,12 @@ void rst(void) {
 		resetFunc();
 	}
 }
-// void re(void){
-// 	if ((byte)Str[3] == 'R' && (byte)Str[4] == 'E') {
-// 		ser0 = EEPROM.read(0);
-// 		ser1 = EEPROM.read(1);
-// 		ser2 = EEPROM.read(2);
-// 		Serial.println("RELOAD DONE");
-// 	}
-// }
 //ac
-void ch(void) {
+void acn(void) {
 	if ( (byte)Str[3] == 'A'  &&  (byte)Str[4] == 'C'&&(byte)Str[5] == '='&& (byte)Str[6] != ""&& (byte)Str[7] != ""&&ser1==255&&ser2==255) {
-		int p1=unascii((byte)Str[6]);
-		EEPROM.write(1, p1);
-		int  p2=unascii((byte)Str[7]);
-		EEPROM.write(2, p2);
-		Serial.print(p1);Serial.print(p2); Serial.println(" AC MODE ON");
+		EEPROM.write(1, unascii((byte)Str[6]));
+		EEPROM.write(2, unascii((byte)Str[7]));
+		Serial.print(unascii((byte)Str[6]));Serial.print(unascii((byte)Str[7])); Serial.println(" AC MODE ON");
 		acmode = 1;
 	}
 }
@@ -137,91 +84,50 @@ void ch(void) {
 void uart(void) {
 	if ((byte)Str[3] == 'U' && (byte)Str[4] == 'A' && (byte)Str[5] == 'R' && (byte)Str[6] == 'T') {
 		if ((byte)Str[7] == '=') {
-			if ((byte)Str[8] != "") {
-				nm=unascii((byte)Str[8]);
-				// sped = btm *pow(2,nm); //'0'=300,'1'=600 ,'2'=1200 '4'=2400
-			}
-			Serial.print(btm *pow(2,nm));
-			Serial.println(" SETTING UART SPEED DONE");
-			EEPROM.write(0, nm);
+			if ((byte)Str[8] != "") // sped = btm *pow(2,nm); //'0'=300,'1'=600 ,'2'=1200 '4'=2400
+			Serial.print(btm *pow(2,unascii((byte)Str[8])));	Serial.println(" SETTING UART SPEED DONE");
+			EEPROM.write(0, unascii((byte)Str[8]));
 		}
-		else         Serial.println(sped);
+		else   Serial.println(sped);
 	}
 }
 //无响应内部命令
 void ack(void){//统计响应次数及相应顺延
-	if (newflag == 1)   {
+	if (ser1!=255&&ser2!=255)   {
 		delay(ser1 * 100 + ser2 * 10);
 		Serial.print(ser1);Serial.print(ser2);Serial.print(":");
-		count++;
 	}
-	else{
-		
-	}
+	else;
+	
 }
 //d
 void d(void) {//例子:25+D13E 25节点d13号输出高
 	if ( (byte)Str[3] == 'D'  && (byte)Str[4] != ""&& (byte)Str[5]!= "" ) {
-		if ((byte)Str[6]== 'E')  {
-			digitalWrite((unascii((byte)Str[4])*10+unascii((byte)Str[5])),1);
-
-		}
-		if ((byte)Str[6]== 'F')  {
-			digitalWrite((unascii((byte)Str[4])*10+unascii((byte)Str[5])),0);
-		}
-		
+		if ((byte)Str[6]== 'E') digitalWrite((unascii((byte)Str[4])*10+unascii((byte)Str[5])),1);
+		if ((byte)Str[6]== 'F')  digitalWrite((unascii((byte)Str[4])*10+unascii((byte)Str[5])),0);
 		if ((byte)Str[6]== "R") {
 			ack();
 			Serial.println(digitalRead((unascii((byte)Str[4])*10+unascii((byte)Str[5]))));//这个有响应
-			
 		}
 	}
 }
-
-
 //过程函数
-byte ascii(byte pic) {
-	return pic + 48;
-}
-
-byte unascii(byte tok) {
-	return tok - 48;
-}
-
-// 		void ask(int detime) {
-// 			if (detime == 1)     delay(ser1 * 100 + ser2 * 10);
-// 			Serial.print(ser1);
-// 			Serial.print(ser2);
-// 		}
-
+byte ascii(byte pic) {return pic + 48;}
+byte unascii(byte tok)	{return tok - 48;}
 void nod(String& Str) {
 	String tempStr = ""; // 声明变量 tempStr，用于临时存储串口输入的数据
-	while (Serial.available()) { // 当串口有数据时，循环执行
-		tempStr += (char)Serial.read();  // 把读取的串口数据，逐个组合到inStr变量里
-	}
+	while (Serial.available())tempStr += (char)Serial.read();  // 当串口有数据时，循环执行 把读取的串口数据，逐个组合到inStr变量里
 	Str = tempStr; // 把引用指针的变量赋值为 tempStr
 }
-
-// 		void sp(void) {
-// 			Serial.println("");
-// 			Serial.println("USE 'HELP' FOR MORE");
-//
-// 		}
-
-
-
-
 //底层命令
-
-
-
 void at(void) {//最底层的
 	if ((byte)Str[0] == 'A' && (byte)Str[1] == 'T') {
 		if ((byte)Str[2] == '+') {
-			pps();
+			shown();
 			uart();
 			rst();
-			ch();
+			acn();
+			help();
 		}
 		if ((byte)Str[2] == '?') {
 			Serial.println("'AT+UART=[N]' IS UART SETTING TOOLS,NUMBER MEAN:300*2^N ");
@@ -229,29 +135,12 @@ void at(void) {//最底层的
 		}
 	}
 }
-
-// 		void ac(void) {
-// 		 if ((byte)Str[0] == 'A' && (byte)Str[1] == 'C') {
-// 	    ask(1);
-// 		   Serial.println("AC MODE ON");
-// 		   acmode = 1;
-// 		 }
-// 		}
-
-
-
 void ins(void) {//机器码+
-	if ((byte)Str[0] == '?') {//轮询
-		ack();
-	}
+	if ((byte)Str[0] == '?') ack();//轮询
 	if ((byte)Str[0] == ascii(ser1) && (byte)Str[1] == ascii(ser2)) {
-		// 				if ((byte)Str[2] == '?') {
-		// 					ack();
-		// 					Serial.println(" STAND BY");
-		// 				}
 		if ((byte)Str[2] == '+') {
 			/*	ex();*/
-			pps();
+			shown();
 			rst();
 			uart();
 			/*	re();*/
@@ -259,13 +148,14 @@ void ins(void) {//机器码+
 		}
 	}
 }
-
 void help(void) {
 	if ((byte)Str[0] == 'H' && (byte)Str[1] == 'E' && (byte)Str[2] == 'L' && (byte)Str[3] == 'P')
 	{
-		//    if (acmode==0)
-		Serial.println("AT+UART ; AT+RST ; []+AC ; ? ; AC");
-		//    else
+		if (acmode==0){
+			Serial.print("COMMAND JUST LIKE: AT+[]");//at模式:串口\地址\重启\展示
+			Serial.println("[]COULD BE:SHOW;SERIAL;AC;RST;");
+		}
+		if (acmode==1)
 		Serial.println("[]? ; []+RST ; []+EXIT ; []+SHOW");
 	}
 }
